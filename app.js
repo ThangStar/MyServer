b9c98d0bdb2fe76f3a60bdec31c27fced6184c78
DESKTOP-IAN77K0\ThangVan <nguyenquocthinhit@gmail.com>
2024-08-12 20:27:37 +0700

    main

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+//cors to fix cors origin, body-parser to fix the post value on the server
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log("+1 connection");
+    
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
